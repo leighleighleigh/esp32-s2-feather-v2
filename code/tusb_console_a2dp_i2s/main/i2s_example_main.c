bf17bab6560d57f6b2c51a234ddb79f19edeb894
Leigh Oliver <leighcharlesoliver@gmail.com>
2021-03-14 20:14:14 +1100

    Initial commit.

---
+++ code/tusb_console_a2dp_i2s/main/i2s_example_main.c
@@ -0,0 +1,176 @@
+/* I2S Example
+
+    This example code will output 100Hz sine wave and triangle wave to 2-channel of I2S driver
+    Every 5 seconds, it will change bits_per_sample [16, 24, 32] for i2s data
+
+    This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+    Unless required by applicable law or agreed to in writing, this
+    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+    CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/i2s.h"
+#include "driver/gpio.h"
+#include "esp_system.h"
+#include <math.h>
+
+#include "esp_log.h"
+#include "esp_vfs.h"
+
+#include "tinyusb.h"
+#include "tusb_cdc_acm.h"
+#include "tusb_console.h"
+#include "sdkconfig.h"
+// #include "io_mux_reg.h"
+#include "driver/gpio.h"
+
+
+#define SAMPLE_RATE     (36000)
+#define I2S_NUM         (0)
+#define WAVE_FREQ_HZ    (100)
+#define PI              (3.14159265)
+#define I2S_BCK_IO      (GPIO_NUM_34)
+#define I2S_WS_IO       (GPIO_NUM_33)
+#define I2S_DO_IO       (GPIO_NUM_21)
+#define I2S_DI_IO       (-1)
+#define I2S_MUTE        (GPIO_NUM_7)
+
+#define SAMPLE_PER_CYCLE (SAMPLE_RATE/WAVE_FREQ_HZ)
+
+static const char *TAG = "example";
+
+static void setup_triangle_sine_waves(int bits)
+{
+    int *samples_data = malloc(((bits+8)/16)*SAMPLE_PER_CYCLE*4);
+    unsigned int i, sample_val;
+    double sin_float, triangle_float, triangle_step = (double) pow(2, bits) / SAMPLE_PER_CYCLE;
+    size_t i2s_bytes_write = 0;
+
+    printf("\r\nTest bits=%d free mem=%d, written data=%d\n", bits, esp_get_free_heap_size(), ((bits+8)/16)*SAMPLE_PER_CYCLE*4);
+
+    triangle_float = -(pow(2, bits)/2 - 1);
+
+    for(i = 0; i < SAMPLE_PER_CYCLE; i++) {
+        sin_float = sin(i * 2 * PI / SAMPLE_PER_CYCLE);
+        if(sin_float >= 0)
+            triangle_float += triangle_step;
+        else
+            triangle_float -= triangle_step;
+
+        sin_float *= (pow(2, bits)/2 - 1);
+
+        if (bits == 16) {
+            sample_val = 0;
+            sample_val += (short)triangle_float;
+            sample_val = sample_val << 16;
+            sample_val += (short) sin_float;
+            samples_data[i] = sample_val;
+        } else if (bits == 24) { //1-bytes unused
+            samples_data[i*2] = ((int) triangle_float) << 8;
+            samples_data[i*2 + 1] = ((int) sin_float) << 8;
+        } else {
+            samples_data[i*2] = ((int) triangle_float);
+            samples_data[i*2 + 1] = ((int) sin_float);
+        }
+
+    }
+
+    i2s_set_clk(I2S_NUM, SAMPLE_RATE, bits, 2);
+    //Using push
+    // for(i = 0; i < SAMPLE_PER_CYCLE; i++) {
+    //     if (bits == 16)
+    //         i2s_push_sample(0, &samples_data[i], 100);
+    //     else
+    //         i2s_push_sample(0, &samples_data[i*2], 100);
+    // }
+    // or write
+    i2s_write(I2S_NUM, samples_data, ((bits+8)/16)*SAMPLE_PER_CYCLE*4, &i2s_bytes_write, 100);
+
+    free(samples_data);
+}
+
+void app_main(void)
+{
+    /* Setting TinyUSB up */
+    ESP_LOGI(TAG, "USB initialization");
+
+    tinyusb_config_t tusb_cfg = { 0 }; // the configuration uses default values
+    ESP_ERROR_CHECK(tinyusb_driver_install(&tusb_cfg));
+
+    tinyusb_config_cdcacm_t amc_cfg = { 0 }; // the configuration uses default values
+    ESP_ERROR_CHECK(tusb_cdc_acm_init(&amc_cfg));
+
+    esp_tusb_init_console(TINYUSB_CDC_ACM_0); // log to usb
+    ESP_LOGI(TAG, "USB initialization DONE");
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    // setup mute pin
+    gpio_config_t io_conf;
+    //disable interrupt
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    io_conf.pin_bit_mask = (1ULL<<I2S_MUTE);
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    io_conf.pull_up_en = 1;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+
+    gpio_set_level(I2S_MUTE,1);
+
+    // Setup the MCLK pin
+    // IO18 / U1RXD
+
+    // WRITE_REG(IO_MUX_PIN_CTRL, 0x0);
+    REG_WRITE(PIN_CTRL,0xFFFFF0F0);
+    PIN_FUNC_SELECT(PERIPHS_IO_MUX_DAC_2_U, FUNC_DAC_2_CLK_OUT3);
+    // PIN_FUNC_SELECT(PERIPHS_IO_MUX_U0TXD_U, FUNC_U0TXD_CLK_OUT3);
+    // PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO0_U, FUNC_GPIO0_CLK_OUT1);
+
+    // Direct the clock out
+    // PIN_CTRL[3:0] = 0x0;
+    // PIN_CTRL[11:8] = 0x0;
+
+    //for 36Khz sample rates, we create 100Hz sine wave, every cycle need 36000/100 = 360 samples (4-bytes or 8-bytes each sample)
+    //depend on bits_per_sample
+    //using 6 buffers, we need 60-samples per buffer
+    //if 2-channels, 16-bit each channel, total buffer is 360*4 = 1440 bytes
+    //if 2-channels, 24/32-bit each channel, total buffer is 360*8 = 2880 bytes
+    i2s_config_t i2s_config = {
+        .mode = I2S_MODE_MASTER | I2S_MODE_TX,                                  // Only TX
+        .sample_rate = SAMPLE_RATE,
+        .bits_per_sample = 16,
+        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,                           //2-channels
+        .communication_format = I2S_COMM_FORMAT_STAND_MSB,
+        .dma_buf_count = 6,
+        .dma_buf_len = 60,
+        .use_apll = false,
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1                                //Interrupt level 1
+    };
+    i2s_pin_config_t pin_config = {
+        .bck_io_num = I2S_BCK_IO,
+        .ws_io_num = I2S_WS_IO,
+        .data_out_num = I2S_DO_IO,
+        .data_in_num = I2S_DI_IO                                               //Not used
+    };
+    i2s_driver_install(I2S_NUM, &i2s_config, 0, NULL);
+    i2s_set_pin(I2S_NUM, &pin_config);
+
+    int test_bits = 16;
+    while (1) {
+        setup_triangle_sine_waves(test_bits);
+        vTaskDelay(5000/portTICK_RATE_MS);
+        test_bits += 8;
+        if(test_bits > 32)
+            test_bits = 16;
+
+    }
+
+}
---
+++ code/tusb_console_battery_i2c/main/tusb_console_main.c
@@ -0,0 +1,181 @@
+/* USB Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+// DESCRIPTION:
+// This example contains minimal code to make ESP32-S2 based device
+// recognizable by USB-host devices as a USB Serial Device printing output from
+// the application.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/reent.h>
+#include "esp_log.h"
+#include "esp_vfs.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_console.h"
+#include "esp_vfs_fat.h"
+#include "cmd_system.h"
+// #include "cmd_i2ctools.h"
+#include "driver/i2c.h"
+
+#include "tinyusb.h"
+#include "tusb_cdc_acm.h"
+#include "tusb_console.h"
+#include "sdkconfig.h"
+
+#define BAT_IC_ADDR 0x36
+
+#define I2C_MASTER_TX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
+#define WRITE_BIT I2C_MASTER_WRITE  /*!< I2C master write */
+#define READ_BIT I2C_MASTER_READ    /*!< I2C master read */
+#define ACK_CHECK_EN 0x1            /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS 0x0           /*!< I2C master will not check ack from slave */
+#define ACK_VAL 0x0                 /*!< I2C ack value */
+#define NACK_VAL 0x1                /*!< I2C nack value */
+
+gpio_num_t i2c_gpio_sda = 8;
+gpio_num_t i2c_gpio_scl = 9;
+uint32_t i2c_frequency = 100000;
+i2c_port_t i2c_port = I2C_NUM_0;
+
+static const char *TAG = "example";
+
+
+static esp_err_t i2c_master_driver_initialize(void)
+{
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = i2c_gpio_sda,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_io_num = i2c_gpio_scl,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = i2c_frequency,
+        // .clk_flags = 0,          /*!< Optional, you can use I2C_SCLK_SRC_FLAG_* flags to choose i2c source clock here. */
+    };
+    return i2c_param_config(i2c_port, &conf);
+}
+
+static esp_err_t i2c_master_sensor_test(i2c_port_t i2c_num, uint8_t reg, uint8_t *data_h, uint8_t *data_l)
+{
+    int ret;
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, BAT_IC_ADDR << 1 | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, reg, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    
+    ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    
+    if (ret != ESP_OK) {
+        return ret;
+    }
+    
+    vTaskDelay(30 / portTICK_RATE_MS);
+
+    cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, BAT_IC_ADDR << 1 | READ_BIT, ACK_CHECK_EN);
+    i2c_master_read_byte(cmd, data_h, ACK_VAL);
+    i2c_master_read_byte(cmd, data_l, NACK_VAL);
+    i2c_master_stop(cmd);
+    ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
+
+void task_max17048_read_vcell(void *ignore)
+{
+	uint8_t vcell_h;
+    uint8_t vcell_l;
+
+    uint8_t soc_h;
+    uint8_t soc_l;
+
+    uint8_t crate_h;
+    uint8_t crate_l;
+
+
+	esp_err_t espErr;
+
+    while(true)
+    {
+
+        vTaskDelay(800/portTICK_PERIOD_MS);
+
+        espErr = i2c_master_sensor_test(i2c_port,0x02, &vcell_h,&vcell_l);
+        espErr = i2c_master_sensor_test(i2c_port,0x4, &soc_h,&soc_l);
+        espErr = i2c_master_sensor_test(i2c_port,0x16, &crate_h,&crate_l);
+        
+        float lux = (float)(vcell_h<<8 | vcell_l) * (float)0.078125;
+        printf("%.6f V\n", lux);
+        
+        float chrg = (float)(soc_h<<8 | soc_l) * (float)0.00390625;
+        printf("%.6f CHRG\n", chrg);
+
+        float crate = (float)(crate_h<<8 | crate_l) * (float)0.208;
+        printf("%.6f per hr\n", crate);
+        
+    }
+
+	vTaskDelete(NULL);
+}
+
+void app_main(void)
+{
+    /* Setting TinyUSB up */
+    ESP_LOGI(TAG, "USB initialization");
+
+    tinyusb_config_t tusb_cfg = { 0 }; // the configuration uses default values
+    ESP_ERROR_CHECK(tinyusb_driver_install(&tusb_cfg));
+
+    tinyusb_config_cdcacm_t amc_cfg = { 0 }; // the configuration uses default values
+    ESP_ERROR_CHECK(tusb_cdc_acm_init(&amc_cfg));
+
+    esp_tusb_init_console(TINYUSB_CDC_ACM_0); // log to usb
+    ESP_LOGI(TAG, "USB initialization DONE");
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    i2c_driver_install(i2c_port, I2C_MODE_MASTER, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+    i2c_master_driver_initialize();
+
+    uint8_t address;
+    printf("     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\r\n");
+    for (int i = 0; i < 128; i += 16) {
+        printf("%02x: ", i);
+        for (int j = 0; j < 16; j++) {
+            fflush(stdout);
+            address = i + j;
+            i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+            i2c_master_start(cmd);
+            i2c_master_write_byte(cmd, (address << 1) | WRITE_BIT, ACK_CHECK_EN);
+            i2c_master_stop(cmd);
+            esp_err_t ret = i2c_master_cmd_begin(i2c_port, cmd, 50 / portTICK_RATE_MS);
+            i2c_cmd_link_delete(cmd);
+            if (ret == ESP_OK) {
+                printf("%02x ", address);
+            } else if (ret == ESP_ERR_TIMEOUT) {
+                printf("UU ");
+            } else {
+                printf("-- ");
+            }
+        }
+        printf("\r\n");
+    }
+
+    // Attempt to ask for battery chip data
+    xTaskCreate(&task_max17048_read_vcell, "task_max17048_read_vcell",  2048, NULL, 6, NULL);
+
+    // i2c_driver_delete(i2c_port);
+}
