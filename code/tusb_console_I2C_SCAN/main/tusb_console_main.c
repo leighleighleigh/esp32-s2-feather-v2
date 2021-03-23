7f6d1a01c3e33a7b68e2afb2a2a6b6cf118839ab
Leigh Oliver <leighcharlesoliver@gmail.com>
2021-03-23 20:22:05 +1100

    PWM Outputs babey

--- code/tusb_console_I2C_SCAN/main/tusb_console_main.c
+++ code/tusb_console_I2C_SCAN/main/tusb_console_main.c
@@ -24,57 +24,37 @@
 #include "esp_console.h"
 #include "esp_vfs_fat.h"
 #include "cmd_system.h"
-// #include "cmd_i2ctools.h"
-#include "driver/i2c.h"
 
 #include "tinyusb.h"
 #include "tusb_cdc_acm.h"
 #include "tusb_console.h"
 #include "sdkconfig.h"
 
-#define BAT_IC_ADDR 0x36
+// LEDC/PWM driver
+#include "driver/ledc.h"
 
-#define I2C_MASTER_TX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
-#define I2C_MASTER_RX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
-#define WRITE_BIT I2C_MASTER_WRITE  /*!< I2C master write */
-#define READ_BIT I2C_MASTER_READ    /*!< I2C master read */
-#define ACK_CHECK_EN 0x1            /*!< I2C master will check ack from slave*/
-#define ACK_CHECK_DIS 0x0           /*!< I2C master will not check ack from slave */
-#define ACK_VAL 0x0                 /*!< I2C ack value */
-#define NACK_VAL 0x1                /*!< I2C nack value */
+#define LEDC_LS_TIMER LEDC_TIMER_1
+#define LEDC_LS_MODE LEDC_LOW_SPEED_MODE
+#define LEDC_LS_CH0_GPIO (12)
+#define LEDC_LS_CH0_CHANNEL LEDC_CHANNEL_0
+#define LEDC_LS_CH1_GPIO (14)
+#define LEDC_LS_CH1_CHANNEL LEDC_CHANNEL_1
 
-gpio_num_t i2c_gpio_sda = 8;
-gpio_num_t i2c_gpio_scl = 9;
-uint32_t i2c_frequency = 400000;
-i2c_port_t i2c_port = I2C_NUM_0;
+#define LEDC_TEST_CH_NUM (2) // There are 16 channels, two groups of 8. One groups is High Speed.
+#define LEDC_TEST_DUTY (16383)
+#define LEDC_TEST_FADE_TIME (10000)
 
 static const char *TAG = "example";
-
-
-static esp_err_t i2c_master_driver_initialize(void)
-{
-    i2c_config_t conf = {
-        .mode = I2C_MODE_MASTER,
-        .sda_io_num = i2c_gpio_sda,
-        .sda_pullup_en = GPIO_PULLUP_ENABLE,
-        .scl_io_num = i2c_gpio_scl,
-        .scl_pullup_en = GPIO_PULLUP_ENABLE,
-        .master.clk_speed = i2c_frequency,
-        // .clk_flags = 0,          /*!< Optional, you can use I2C_SCLK_SRC_FLAG_* flags to choose i2c source clock here. */
-    };
-    return i2c_param_config(i2c_port, &conf);
-}
-
 
 void app_main(void)
 {
     /* Setting TinyUSB up */
     ESP_LOGI(TAG, "USB initialization");
 
-    tinyusb_config_t tusb_cfg = { 0 }; // the configuration uses default values
+    tinyusb_config_t tusb_cfg = {0}; // the configuration uses default values
     ESP_ERROR_CHECK(tinyusb_driver_install(&tusb_cfg));
 
-    tinyusb_config_cdcacm_t amc_cfg = { 0 }; // the configuration uses default values
+    tinyusb_config_cdcacm_t amc_cfg = {0}; // the configuration uses default values
     ESP_ERROR_CHECK(tusb_cdc_acm_init(&amc_cfg));
 
     esp_tusb_init_console(TINYUSB_CDC_ACM_0); // log to usb
@@ -82,31 +62,64 @@ void app_main(void)
 
     vTaskDelay(1000 / portTICK_PERIOD_MS);
 
-    i2c_driver_install(i2c_port, I2C_MODE_MASTER, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
-    i2c_master_driver_initialize();
+    // Setup the PWM stuff
+    int ch;
 
-    uint8_t address;
-    printf("     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\r\n");
-    for (int i = 0; i < 128; i += 16) {
-        printf("%02x: ", i);
-        for (int j = 0; j < 16; j++) {
-            fflush(stdout);
-            address = i + j;
-            i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-            i2c_master_start(cmd);
-            i2c_master_write_byte(cmd, (address << 1) | WRITE_BIT, ACK_CHECK_EN);
-            i2c_master_stop(cmd);
-            esp_err_t ret = i2c_master_cmd_begin(i2c_port, cmd, 50 / portTICK_RATE_MS);
-            i2c_cmd_link_delete(cmd);
-            if (ret == ESP_OK) {
-                printf("%02x ", address);
-            } else if (ret == ESP_ERR_TIMEOUT) {
-                printf("UU ");
-            } else {
-                printf("-- ");
-            }
-        }
-        printf("\r\n");
+    /*
+     * Prepare and set configuration of timers
+     * that will be used by LED Controller
+     */
+    ledc_timer_config_t ledc_timer = {
+        .duty_resolution = LEDC_TIMER_14_BIT, // resolution of PWM duty 
+        .freq_hz = 1000,                      // frequency of PWM signal
+        .speed_mode = LEDC_LS_MODE,           // timer mode
+        .timer_num = LEDC_LS_TIMER,           // timer index
+        .clk_cfg = LEDC_AUTO_CLK,             // Auto select the source clock
+    };
+
+    // Set configuration of timer0 for high speed channels
+    ledc_timer_config(&ledc_timer);
+    /*
+     * Prepare individual configuration
+     * for each channel of LED Controller
+     * by selecting:
+     * - controller's channel number
+     * - output duty cycle, set initially to 0
+     * - GPIO number where LED is connected to
+     * - speed mode, either high or low
+     * - timer servicing selected channel
+     *   Note: if different channels use one timer,
+     *         then frequency and bit_num of these channels
+     *         will be the same
+     */
+    ledc_channel_config_t ledc_channel[LEDC_TEST_CH_NUM] = {
+        {.channel = LEDC_LS_CH0_CHANNEL,
+         .duty = 0,
+         .gpio_num = LEDC_LS_CH0_GPIO,
+         .speed_mode = LEDC_LS_MODE,
+         .hpoint = 0,
+         .timer_sel = LEDC_LS_TIMER},
+        {.channel = LEDC_LS_CH1_CHANNEL,
+         .duty = 0,
+         .gpio_num = LEDC_LS_CH1_GPIO,
+         .speed_mode = LEDC_LS_MODE,
+         .hpoint = 0,
+         .timer_sel = LEDC_LS_TIMER},
+    };
+
+    // Set LED Controller with previously prepared configuration
+    for (ch = 0; ch < LEDC_TEST_CH_NUM; ch++)
+    {
+        ledc_channel_config(&ledc_channel[ch]);
     }
 
+    // Initialize fade service.
+    ledc_fade_func_install(0);
+
+    printf("1. LEDC fade up to duty = %d\n", LEDC_TEST_DUTY);
+    for (ch = 0; ch < LEDC_TEST_CH_NUM; ch++)
+    {
+        ledc_set_fade_with_time(ledc_channel[ch].speed_mode,ledc_channel[ch].channel, LEDC_TEST_DUTY, LEDC_TEST_FADE_TIME);
+        ledc_fade_start(ledc_channel[ch].speed_mode,ledc_channel[ch].channel, LEDC_FADE_NO_WAIT);
+    }
 }
