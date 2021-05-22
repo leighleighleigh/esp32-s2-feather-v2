bfef81f27225afd59aa8c5a4abfcdcccf476bb4e
Leigh Oliver <leighcharlesoliver@gmail.com>
2021-05-22 19:09:32 +1000

    Made message list better with delays

--- code/bm83-uart-python/decoding_example.py
+++ code/bm83-uart-python/decoding_example.py
@@ -31,28 +31,24 @@ def print_msg(msg):
 ### Create a list of message to send
 # And send one off after the init messages (3) recieved
 msgsRecieved = 0
-msgsRecievedSendThresh = 3
+msgsRecievedSendThresh = 0
 cmds_to_send = []
 
 ### Turn on commands
+cmds_to_send.append(driver.assemble_message(0x02,[0x00,0x53]))
+cmds_to_send.append(driver.assemble_message(0x02,[0x00,0x54]))
+cmds_to_send.append([3]) # Delay of 3 sec
 cmds_to_send.append(driver.assemble_message(0x02,[0x00,0x51]))
 cmds_to_send.append(driver.assemble_message(0x02,[0x00,0x52]))
 
 ### Link status
-cmds_to_send.append(driver.assemble_message(0x0D,[0x0]))
+# cmds_to_send.append(driver.assemble_message(0x0D,[0x0]))
 
 ### Play music
+cmds_to_send.append([5])
 cmds_to_send.append(driver.assemble_message(0x02,[0x00,0x32]))
-cmds_to_send.append(driver.assemble_message(0x02,[0x00,0x34]))
-cmds_to_send.append(driver.assemble_message(0x02,[0x00,0x34]))
-cmds_to_send.append(driver.assemble_message(0x02,[0x00,0x34]))
-cmds_to_send.append(driver.assemble_message(0x02,[0x00,0x34]))
-cmds_to_send.append(driver.assemble_message(0x02,[0x00,0x34]))
-cmds_to_send.append(driver.assemble_message(0x02,[0x00,0x34]))
-cmds_to_send.append(driver.assemble_message(0x02,[0x00,0x34]))
-cmds_to_send.append(driver.assemble_message(0x02,[0x00,0x34]))
-cmds_to_send.append(driver.assemble_message(0x02,[0x00,0x34]))
-cmds_to_send.append(driver.assemble_message(0x02,[0x00,0x34]))
+### Skip track
+cmds_to_send.append([3])
 cmds_to_send.append(driver.assemble_message(0x02,[0x00,0x34]))
 
 ### Read BTM Version
@@ -71,21 +67,17 @@ while True:
 
     # Send a message
     if(msgsRecieved >= msgsRecievedSendThresh and len(cmds_to_send) != 0):
-        # time.sleep(1)
-        # print("HOLD MSB")
-        # time.sleep(3)
         # Pop next message of queue
         msgToSend = cmds_to_send.pop(0)
         
-        # Send it
-        print("TX: {}".format(str([hex(x) for x in msgToSend])))
-        ser.write(bytes(msgToSend))
-        
-        # Increase thresh
-        msgsRecievedSendThresh+=1
-        
-        # Wait
-        if msgsRecievedSendThresh > 4:
-            time.sleep(5)
+        # If len == 1, delay for X seconds
+        if(len(msgToSend) == 1):
+            time.sleep(msgToSend[0])
+        else:
+            # Send it
+            print("TX: {}".format(str([hex(x) for x in msgToSend])))
+            ser.write(bytes(msgToSend))    
+            # Increase thresh
+            msgsRecievedSendThresh+=1
 
 ser.close()
